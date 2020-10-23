349f5bf2dcd3bcc43004a2a6aa79e52f8be4eb80
Anil <gupta.nil1404@gmail.com>
2020-10-23 15:04:45 +0530

    Custom url parser implementation to parse with or without host

--- src/helper/UrlParser.js
+++ src/helper/UrlParser.js
@@ -1,16 +1,25 @@
-const URL = require('url');
+const { parse } = require('path');
+const { URL } = require('url');
 
 // for few url there is no host present in it, so we need to prepend https://medium.com 
 // before the url for those cases
 
 const checkHostPresent = url => {
-	
+	return url[0] === '/' ? false : true;
 }
 
 var UrlParser = function (url) {
-	const parseUrl = URL.parse(url);
-	console.log(parseUrl);
-	return parseUrl;
+	const parseUrl = checkHostPresent(url) ? new URL(url) : new URL(url, 'https://medium.com');
+	return {
+		origin: parseUrl.origin,
+		host: parseUrl.host,
+		hostname: parseUrl.hostname,
+		pathname: parseUrl.pathname,
+		params: parseUrl.searchParams
+	};
 }
 
-module.exports = UrlParser;
+module.exports = {
+	UrlParser,
+	checkHostPresent
+}
--- test/UrlParser.test.js
+++ test/UrlParser.test.js
@@ -1,9 +1,19 @@
 const assert = require('assert');
-const UrlParser = require('../src/helper/UrlParser');
+const { UrlParser, checkHostPresent } = require('../src/helper/UrlParser');
 
 describe('Url Parse Test', () => {
 	it('should return correct hostname', () => {
 		const parseurl = UrlParser('https://medium.com/about/me?name=anil&age=24');
 		assert.strictEqual(parseurl.hostname, 'medium.com');
+	});
+	it('should return medium hostname for null host', () => {
+		const parseUrl = UrlParser('/about/me?name=anil&age=24');
+		assert.strictEqual(parseUrl.hostname, 'medium.com');
+	})
+});
+
+describe('Hostname Present Check', () => {
+	it('should return true for no hostname in url', () => {
+		assert.strictEqual(checkHostPresent('/about/me?name=anil&age=24'), false);
 	})
 })
