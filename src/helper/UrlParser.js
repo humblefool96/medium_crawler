fae34f1f48af3b511d7e81a543831f39053b5205
Anil <gupta.nil1404@gmail.com>
2020-10-23 14:27:02 +0530

    test setup for urlparser

--- src/helper/UrlParser.js
+++ src/helper/UrlParser.js
@@ -2,6 +2,11 @@ const URL = require('url');
 
 // for few url there is no host present in it, so we need to prepend https://medium.com 
 // before the url for those cases
+
+const checkHostPresent = url => {
+	
+}
+
 var UrlParser = function (url) {
 	const parseUrl = URL.parse(url);
 	console.log(parseUrl);
--- test/UrlParser.test.js
+++ test/UrlParser.test.js
@@ -3,8 +3,7 @@ const UrlParser = require('../src/helper/UrlParser');
 
 describe('Url Parse Test', () => {
 	it('should return correct hostname', () => {
-		// const parseurl = UrlParser('https://medium.com/about/me?name=anil&age=24');
-		// assert.equal(parseurl.hostname, 'medium.com');
-		assert.equal(2 + 2, 4);
+		const parseurl = UrlParser('https://medium.com/about/me?name=anil&age=24');
+		assert.strictEqual(parseurl.hostname, 'medium.com');
 	})
 })
